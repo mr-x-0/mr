593645523096bbfbad9ae292d23872417ce9b443
mr-x-0 <urfavmody@gmail.com>
2025-01-01 19:49:28 +0200

    Update mody.py

--- mody.py
+++ mody.py
@@ -2,8 +2,8 @@ import os
 
 
 class Mody(object):
-    TOKEN = os.environ.get("ELHYBA", "")
+    TOKEN = os.environ.get("TOKEN", "")
     
-    OWNER_ID = os.environ.get("OWNER", "6581896306")
+    OWNER_ID = os.environ.get("OWNER_ID", "6581896306")
 
     
